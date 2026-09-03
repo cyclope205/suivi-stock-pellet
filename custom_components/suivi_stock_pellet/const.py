b16e0bf05b4e570ff7f0f863ca3c357961318520
cyclope205 <71901181+cyclope205@users.noreply.github.com>
2026-09-03 08:56:08 +0200

    Add new constants for entry editing and indexing

--- custom_components/suivi_stock_pellet/const.py
+++ custom_components/suivi_stock_pellet/const.py
@@ -17,10 +17,12 @@ STORAGE_VERSION = 1
 SERVICE_LOG_CONSUMPTION = "log_consumption"
 SERVICE_LOG_PURCHASE = "log_purchase"
 SERVICE_UNDO_LAST_ENTRY = "undo_last_entry"
+SERVICE_EDIT_ENTRY = "edit_entry"
 
 ATTR_QTY_BAGS = "qty_bags"
 ATTR_PRICE_EUR = "price_eur"
 ATTR_DATE = "date"
+ATTR_INDEX = "index"
 
 ENTRY_TYPE_CONSUMPTION = "consumption"
 ENTRY_TYPE_PURCHASE = "purchase"
